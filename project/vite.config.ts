39ba9fc31bd2f30a88f8e03dbb5ffd8173ea5deb
Sai Lahari <suraganilahari72@gmail.com>
2025-04-24 02:19:23 -0400

    Configure for GitHub Pages deployment

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/project-bolt-callingconventions/",
+  base: "/FinalProject/",
   plugins: [react()],
   build: {
     outDir: "dist"
